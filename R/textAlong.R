72c3d63e3a4d8d48ee6586784d2732068df7b5f4
noriakis <31095487+noriakis@users.noreply.github.com>
2023-05-08 23:46:18 +0900

    Update textAlong.R

--- R/textAlong.R
+++ R/textAlong.R
@@ -2,7 +2,7 @@ textAlongGrob <- function(label, x = unit(0.5, 'npc'), y = unit(0.5, 'npc'),
                           just = 'centre', hjust = NULL, vjust = NULL, rot = 0,
                           check.overlap = FALSE, rot.type = 'rot', x0 = 0,
                           y0 = 0, x1 = 0, y1 = 0, force.rot = TRUE, dodge = NULL,
-                          push = NULL,
+                          push = NULL, bg.colour="white", bg.r=0.1,
                           default.units = 'npc', name = NULL, gp = gpar(),
                           vp = NULL) {
   if (!is.unit(x)) {
@@ -12,10 +12,11 @@ textAlongGrob <- function(label, x = unit(0.5, 'npc'), y = unit(0.5, 'npc'),
     y <- unit(y, default.units)
   }
   if (rot.type == 'rot') {
-    textGrob(
+    shadowtext::shadowtextGrob(
       label = label, x = x, y = y, just = just, hjust = hjust,
       vjust = vjust, rot = rot, check.overlap = check.overlap,
-      default.units = default.units, name = name, gp = gp, vp = vp
+      default.units = default.units, name = name, gp = gp, vp = vp,
+      bg.colour=bg.colour, bg.r=bg.r
     )
   } else {
     if (!rot.type %in% c('along', 'across')) {
@@ -33,13 +34,50 @@ textAlongGrob <- function(label, x = unit(0.5, 'npc'), y = unit(0.5, 'npc'),
     if (!is.unit(y1)) {
       y1 <- unit(y1, default.units)
     }
-    grob(
-      label = label, x = x, y = y, just = just, hjust = hjust,
-      vjust = vjust, rot.type = rot.type, x0 = x0, y0 = y0, x1 = x1,
-      y1 = y1, force.rot = force.rot, dodge = dodge, push = push,
+    x0 <- convertX(x0, 'mm', TRUE)
+    y0 <- convertY(y0, 'mm', TRUE)
+    x1 <- convertX(x1, 'mm', TRUE)
+    y1 <- convertY(y1, 'mm', TRUE)
+    xpos <- convertX(x, 'mm', TRUE)
+    ypos <- convertY(y, 'mm', TRUE)
+    angle <- edge_angle(x0, y0, x1, y1)
+    if (rot.type == 'across') {
+      angle <- angle - 90
+    }
+    if (!is.null(dodge)) {
+      dodge <- convertHeight(dodge, 'mm', TRUE)
+      dodge_angle <- (angle + 90) / 360 * 2 * pi
+      dodge_x <- cos(dodge_angle) * dodge
+      dodge_y <- sin(dodge_angle) * dodge
+      xpos <- xpos + dodge_x
+      ypos <- ypos + dodge_y
+    }
+    if (!is.null(push)) {
+      push <- convertHeight(push, 'mm', TRUE)
+      push_angle <- angle / 360 * 2 * pi
+      push_x <- cos(push_angle) * push
+      push_y <- sin(push_angle) * push
+      xpos <- xpos + push_x
+      ypos <- ypos + push_y
+    }
+    if (force.rot) {
+      fix <- angle > 90 & angle < 270
+      angle[fix] <- angle[fix] + 180
+    }
+    shadowtext::shadowtextGrob(
+     label = label, x = x, y = y, just = just, hjust = hjust,
+      vjust = vjust, rot=angle,
       check.overlap = check.overlap, name = name, gp = gp, vp = vp,
-      cl = 'textalong'
-    )
+      bg.colour=bg.colour, bg.r=bg.r 
+      )
+    # grob(
+    #   label = label, x = x, y = y, just = just, hjust = hjust,
+    #   vjust = vjust, rot.type = rot.type, x0 = x0, y0 = y0, x1 = x1,
+    #   y1 = y1, force.rot = force.rot, dodge = dodge, push = push,
+    #   check.overlap = check.overlap, name = name, gp = gp, vp = vp,
+    #   bg.colour=bg.colour, bg.r=bg.r,
+    #   cl = 'textalong'
+    # )
   }
 }
 #' Text angled according to line
@@ -85,6 +123,7 @@ makeContent.textalong <- function(x) {
     label = x$label, x = unit(xpos, 'mm'), y = unit(ypos, 'mm'),
     just = x$just, hjust = x$hjust, vjust = x$vjust, rot = angle,
     check.overlap = x$check.overlap, name = x$name, gp = x$gp, vp = x$vp,
-    cl = 'text'
+    cl="text"
+    # bg.colour=x$bg.colour, bg.r=x$bg.r
   )
 }
